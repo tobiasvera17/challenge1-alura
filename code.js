f4cd633eda2393b0131aceb4a2d2a4ed025d192d
Tobias Vera <tobiasvera2011@gmail.com>
2023-06-23 17:26:20 -0400

    agregar funciones a los botones

--- code.js
+++ code.js
@@ -1,13 +1,17 @@
 const text = document.querySelector(".text-input")
 const encryptButton = document.querySelector(".text-buttons-encrypt")
 const decryptButton = document.querySelector(".text-buttons-decrypt")
+const result_img = document.querySelector("#result-img")
+const result_text = document.querySelector(".result-text")
+const result_text_cont = document.querySelector("#result-text")
+const copyButton = document.querySelector("#result-text-button-copy")
 
 const encrypt = () => {
-    console.log(text.value)
+
     let encrypted_text="";
     const text_to_encrypt = text.value.split("")
-    console.log(text_to_encrypt)
-    text_to_encrypt.map((item, index) => {
+
+    text_to_encrypt.map((item) => {
         if(item == 'e'){
             encrypted_text += "enter"        
         }
@@ -27,12 +31,41 @@ const encrypt = () => {
             encrypted_text += item
         }
 
-        return encrypted_text
     })
-
+        result_img.setAttribute("hidden", true)
+        result_text.textContent = encrypted_text
+        result_text_cont.removeAttribute("hidden")
 }
+
+const decrypt = () => {
+    let codes = [['enter', 'e'], ['imes','i'], ['ai', 'a'], ['ober', 'o'], ['ufat', 'u']] 
+    const text_to_decrypt = text.value
+    let decrypted_text= text_to_decrypt;
+
+    codes.map((item) => {
+        var regex = new RegExp(item[0], "g");
+        decrypted_text = decrypted_text.replace(regex, item[1])
+    })
+    result_img.setAttribute("hidden", true)
+    result_text.textContent = decrypted_text
+    result_text_cont.removeAttribute("hidden")
+}
+
+const copy = () => {
+
+    navigator.clipboard.writeText(result_text.textContent);
+} 
 
 encryptButton.addEventListener('click', ()=> {
     encrypt()
 
+})
+
+decryptButton.addEventListener('click', ()=> {
+    decrypt()
+
+})
+
+copyButton.addEventListener('click',()=>{
+    copy()
 })
